e137c6562b91b59d03938b471cbecc9a264fbad3
Maria Camila Ortega <148597026+mcamilaortega@users.noreply.github.com>
2024-01-14 16:52:24 +0000

    missing icon

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,42 @@
 import React from "react";
-import {SecondsCounter} from "./SecondsCounter.jsx"
+
 
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<SecondsCounter/>
+		<div className="d-flex justify-content-center calendar">
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds/100000)%10
+				}
+			</h1>
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds/10000)%10
+				}
+			</h1>
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds/1000)%10
+				}
+			</h1>
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds/100)%10
+				}
+			</h1>
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds/10)%10
+				}
+			</h1>
+			<h1 className="digit">
+				{
+					Math.floor (props.seconds)%10
+				}
+			</h1>
+			
 		</div>
 	);
 };
